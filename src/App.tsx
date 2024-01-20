32beeb0a6443f44be442d3919653aeac7e0e0154
MrAmanJoshi <113497951+MrAmanJoshi@users.noreply.github.com>
2024-01-20 14:50:53 +0000

    remove comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ShowDetailPage from "./Pages/ShowDetails.Page";
 import ShowListPage from "./Pages/ShowsList.Page";
-// need to fix UI bugs
+
 function App() {
   return (
     <React.Fragment>
-        <Routes>
-          <Route path="/" element={<ShowListPage />} />
-          <Route path="show/:show_id" element={<ShowDetailPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<ShowListPage />} />
+        <Route path="show/:show_id" element={<ShowDetailPage />} />
+      </Routes>
     </React.Fragment>
   );
 };
